8a32fde00e8ce009ca457848c12dc056194d3605
truejulosdu13 <julosdu13@yahoo.fr>
2021-10-01 09:29:24 +0200

    clean_up

--- auto_qchem_2021/dash_app/app.py
+++
@@ -1,8 +0,0 @@
-import dash
-import dash_bootstrap_components as dbc
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SANDSTONE],
-                title='Auto-QChem DB',
-                update_title="Loading...")
-server = app.server
-app.config.suppress_callback_exceptions = True 
